bcc9a402887d80678b8dcf7ec460f7e32cd85065
Skyhawk7583 <skyhawkingsplays@gmail.com>
2022-01-08 12:26:46 +0000

    added DB connect

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('Wello Horld?'));
-
-
-
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
