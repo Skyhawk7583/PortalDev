cfef6069890e521a47fcbcc53c8b85ed0152a4ea
Skyhawk7583 <skyhawkingsplays@gmail.com>
2022-01-10 11:48:59 +0000

    Middileware Auth, users route, users model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 
-// @route        GET api/users
-// @descriptions Test route
+// User Model
+const User = require('../../models/User');
+
+// @route        POST api/users
+// @descriptions Register User route
 // @access       Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 8 or more characters').isLength({ min: 6 }),
+    check('pin', 'Pin is required').not().isEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password, pin} = req.body;
+
+    try {
+        // See if user exists
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+        }
+
+        // Get Users pin
+        user = new User({
+            name,
+            email,
+            pin,
+            password
+        });
+
+        // Encrypt Password 
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
+        // Return JsonWebToken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Internal Server Error');
+    }
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Middileware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Wello Horld?'));
 
 // Define Routes
